4c829ab8124b9af20f1e003f0e7bd9cdc5983168
TylerJulian <tyler.james.julian@ou.edu>
2021-07-09 09:10:12 -0500

    reads token files and connects to lichess session

--- chs.py
+++ chs.py
@@ -1,3 +1,23 @@
 import chess
-board = chess.Board()
-print(board)
+import lichess.api
+import berserk
+def main():
+    #requires a api token from https://lichess.org/account/oauth/token
+    #create a file named lichess.token and copy your api token into it.
+    with open('./lichess.token') as f:
+        token = f.read()
+
+    #connects to lichess using lichess.
+    session = berserk.TokenSession(token)
+    client = berserk.Client(session)
+
+
+
+
+    #board = chess.Board()
+    #print(board)
+
+
+
+if __name__ == "__main__":
+    main()
