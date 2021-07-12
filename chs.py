284f1776cfc5e76b2fbbbc63e53ef0bf59c1a044
TylerJulian <tyler.james.julian@ou.edu>
2021-07-12 15:08:37 -0500

    began implementation of game search. Seek is currently not working

--- chs.py
+++ chs.py
@@ -8,39 +8,58 @@ import datetime
 
 def main():
     #connect to lichess
-    session, client = lichess_client.connect_client()
+    session, client, board = lichess_client.connect_client()
     # get username
     info = client.account.get()
     username = info['id']
     print('Your user id: ', username)
     
-    print("Press enter to start")
-    input()
+    print("Which mode would you like to start with?")
+    print("1: challenge")
+    print("2: Rapid 10 min")
+    print("3: classical 30 min")
+    print("4: bots")
+    mode = input()
+
     print("waiting for challenge")
+    
 
-    for event in client.board.stream_incoming_events():
-        if event['type'] == 'challenge':
-            id = event['challenge']['id']
-            client.challenges.accept(id)
-            print("challenge found")
+    if (mode == '1'):
+        for event in client.board.stream_incoming_events():
+            if event['type'] == 'challenge':
+                id = event['challenge']['id']
+                client.challenges.accept(id)
+                print("challenge found")
 
-            current_game = lichess_client.game(client, id, username, (id == client.games.export(event['challenge']['id'])['players']['white']['user']['id']),10)
+                current_game = lichess_client.game(client, id, username, (username == client.games.export(event['challenge']['id'])['players']['white']['user']['id']),10)
 
-            current_game.start()
+                current_game.start()
 
-            exit
+                exit()
+    if (mode == '2'):
+        test = board.stream_incoming_events()
+        for event in test:
+            print("searching")
+            print(berserk.clients.Board.seek(client.board, 10, 0))
+            print("found")
+            print("game")
+            if event['type'] == 'gameStart':
+                id = event['game']['id']
+                print("id: ", id)
+                print("game found")
 
-            
-            
-        elif event['type'] == 'gameStart':
-            print("gamestart")
-            
+                current_game = lichess_client.game(client, id, username, (username == client.games.export(event['game']['id'])['players']['white']['user']['id']),10)
+
+                current_game.start()
+
+                exit()
+
+
 
 
             
     #board = chess.Board()
     #print(board)
-
 
 
 if __name__ == "__main__":
--- lichess_client.py
+++ lichess_client.py
@@ -3,6 +3,8 @@ import berserk
 import datetime
 from time import sleep
 
+from lichess.api import current_game
+
 
 def connect_client():
     #requires a api token from https://lichess.org/account/oauth/token
@@ -12,10 +14,10 @@ def connect_client():
 
     #connects to lichess using lichess.
     session = berserk.TokenSession(token)
-    client = berserk.Client(session)
+    client = berserk.clients.Client(session)
     board = berserk.clients.Board(session)
 
-    return session, client
+    return session, client, board
 
 class game():
     def __init__(self, client, id, username, is_white, start_time):
@@ -27,29 +29,51 @@ class game():
         self.clock = (datetime.datetime(2000, 1,1,0,start_time,0), datetime.datetime(2000, 1,1,0,start_time,0))
         self.current_board = chess.Board()
         self.stream = self.client.board.stream_game_state(self.id)
+        self.current_command = ""
+        self.your_turn = False
+        self.turn_count = 0
 
     def start(self):
         if self.is_white == True:
+            self.your_turn = True
             print("white")
         else:
+            self.your_turn = False
             print("black")
+        self.update_display()
+        for event in self.stream:
+            print("loop")
+            
+            if event['type'] == 'gameState':
+                print("gamestate")
+                print(event["moves"])
+                if(self.your_turn != True):
+                    if(event["moves"].split()[-1] == self.current_command):
+                        pass
+                    else:
+                        self.current_board.push_uci(event["moves"].split()[-1])
+                        self.update_display()
+                        self.turn_count = self.turn_count + 1
+                        self.your_turn = True
+                if(self.your_turn == True): 
+                    self.parse_input()
+                    self.update_display()
+                    self.your_turn = False
+                    self.turn_count = self.turn_count + 1
 
+
+    def update_display(self):
+        #prints the board and adds a gap between next state.
         print(self.current_board)
         print("")
-        command = input("your move?")
-        self.client.board.make_move(self.id, command)
-        current_move = chess.Move.from_uci(command)
+
+    def parse_input(self):
+        self.current_command = input("your move?")
+        if(self.current_command == 'quit'):
+            self.client.board.resign_game(self.id)
+            exit()
+        self.client.board.make_move(self.id, self.current_command)
+        current_move = chess.Move.from_uci(self.current_command)
         self.current_board.push(current_move)  # Make the move
         self.client.board.post_message(self.id,"hello")
-
-        for event in self.stream:
-            if event['type'] == 'gameState':
-                print(self.current_board)
-                self.current_board.push_uci(event["moves"].split()[-1])
-                print(self.current_board)
-                print("")
-                sleep(10)
-                self.client.board.resign_game(self.id)
-                print("lost game")
-
-
+        self.update_display()
